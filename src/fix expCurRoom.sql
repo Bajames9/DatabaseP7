2bfc08d6e64b662f5e3d48c73598ef0ba50a357c
djuilfs <158517157+danieljuilfs@users.noreply.github.com>
2026-04-23 19:51:44 -0400

    Create fix expCurRoom.sql

---
+++ src/fix expCurRoom.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE TRIGGER "expCurRoomCheckTreasureGrab"
+    BEFORE UPDATE OF expID ON TREASURES
+    FOR EACH ROW
+    DECLARE
+        connection Explorers.room_ID%type;
+    BEGIN
+
+        IF :NEW.EXPID iS NULL THEN
+            RETURN;
+        END IF; 
+        SELECT room_ID 
+        INTO connection
+        FROM EXPLORERS
+        WHERE :New.EXPID = EXPLORERS.EXPID;
+
+        IF connection IS NULL OR connection != :Old.roomid THEN
+            RAISE_APPLICATION_ERROR(-20003, 'Item is not in same room as explorer');
+        END IF;
+    END;
+
+/
