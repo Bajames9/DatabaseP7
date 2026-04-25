23ed2495e6f13a3f2e82cf4f78429e2794133cfa
kadehamilton68090-hue <kadehamilton68090@gmail.com>
2026-04-25 16:24:11 -0400

    Add files via upload

---
+++ src/test.java
@@ -0,0 +1,89 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class test
+{
+    Explorer explorer1;
+    DBConnection db = new DBConnection();
+
+    //Create
+    // name >= 3 -Pass
+     public void testGamePass()
+     {
+
+        String[] input = {"new", "Joseph", "troll1"}; 
+        if (input.length >= 3)
+        {
+            explorer1 = db.getExplorer(input[1],input[2]);
+            System.out.println("Passed");
+        }
+        if (explorer1 != null)
+        {
+            System.out.println(explorer1.getName() + " is ready to play");
+            displayGameState();
+
+        }
+        else
+        {
+            System.out.println("Failed")
+        }
+    }
+        
+        
+    public void testGameFail1()
+    {
+        String[] input = {"new", "Tom"};
+         if (input.length >= 3)
+        {
+            System.out.println("Pass");
+        }
+        else
+            {
+                System.out.println("Failed");
+            }
+       
+
+    }
+
+    public void testNewFail2()
+    {
+        String[] input = {null,null,null};
+        if (explorer1 != null)
+        {
+            System.out.println("Pass");
+            
+        }
+        else
+        {
+                System.out.println("Failed");
+        }
+    }
+
+    
+    //name<3 - Fail
+    //Move
+    //Move to a room that exists and is connected to your room - Pass
+    //Move to a room that isnt connected - Fail
+    //Move to a room that doesnt exist - Fail
+
+    //Grab
+    //Grab treasure that does exsist and is in the same room as exp - Paaa
+    // Grab treasure that is not in same room as exp - Fail
+    //Grab treasure that doesnt exist - Fail
+    //Grab a treasure that fails the wtcnt, too much weight - Fail
+    //Grab a treasure that fails the bgcnt, too many items - Fail
+    //Grab treasure 
+    //Drop
+    //Drop treasure that is in inventory - Pass
+    //-Drop treasure that isnt in inventory - Fail
+    
+
+
+
+
+
+
+
+
+
+}
